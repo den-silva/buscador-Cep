3186add7bd7bf9e2d83adb84f16dc117544c3927
den-silva <dmurilosilva@hotmail.com>
2022-09-20 23:20:09 -0300

    HTML
    Criação da estrutura da página em HTML

--- buscador_cep/src/App.js
+++ buscador_cep/src/App.js
@@ -2,8 +2,25 @@
 function App() {
   return (
     <div className="App">
-      Início
+      <h1 className="title">Buscador CEP</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Digite seu CEP"
+        />
+        <button className="buttonSearch">
+            Pesquisar 
+        </button>
+      </div>
+      <main className="main">
+        <h2>CEP: 12345-678</h2>
+        
+        <span>Rua Vista Alegre, 60</span>
+        <span>Complemento: Bloco 2, Apto 71</span>
+        <span>Demarchi</span>
+        <span>São Bernardo do Campo - SP</span>
 
+      </main>
 
     </div>
   );

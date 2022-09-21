ecd89dac0e1bc4f551f45147d7d8699cc69caa53
den-silva <dmurilosilva@hotmail.com>
2022-09-21 00:58:08 -0300

    Estilo da página
    Importação da dependência react-icons, estilização da página, implementação de media queries.

--- buscador_cep/src/App.js
+++ buscador_cep/src/App.js
@@ -1,28 +1,34 @@
+import { FiSearch} from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1 className="title">Buscador CEP</h1>
-      <div>
+      <div className='containerInput' id='cep'>
         <input
+          
           type="text"
-          placeholder="Digite seu CEP"
+          placeholder="Digite seu CEP aqui..."
         />
         <button className="buttonSearch">
-            Pesquisar 
+            Pesquisar &nbsp;
+            <FiSearch size={15} color="#fff"/> 
         </button>
       </div>
       <main className="main">
         <h2>CEP: 12345-678</h2>
-        
+
         <span>Rua Vista Alegre, 60</span>
         <span>Complemento: Bloco 2, Apto 71</span>
-        <span>Demarchi</span>
-        <span>São Bernardo do Campo - SP</span>
+        <span>Bairro: Demarchi</span>
+        <span>Cidade: São Bernardo do Campo - SP</span>
 
       </main>
-
+      
     </div>
+    
+    
   );
 }
 
